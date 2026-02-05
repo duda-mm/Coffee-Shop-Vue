b7f25091adaba7fca431cdfd403bbc4c0ccdafbf
Duda M <dudamdemedeiros@gmail.com>
2026-02-05 10:10:01 -0300

    adição do local storage

--- src/views/CafesView.vue
+++ src/views/CafesView.vue
@@ -1,6 +1,6 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import ProductCard from '../components/ProductCard.vue'
-
 
 import imgamericano from '@/assets/img/produtos/americano.png'
 import imgmacchiato from '@/assets/img/produtos/macchiato.png'
@@ -8,41 +8,30 @@ import imgflatwhite from '@/assets/img/produtos/flatwhite.png'
 import imgcappuccino from '@/assets/img/produtos/cappuccino.png'
 import graoBaixo from '@/assets/img/background/GraosBottomCafe.png'
 
-const listaCafes = [
-  { 
-    id: 1, 
-    nome: 'AMERICANO',  
-    preco: 'R$ 12,95', 
-    imagem: imgamericano
-  },
-  { 
-    id: 2, 
-    nome: 'MACCHIATO',  
-    preco: 'R$ 20,75', 
-    imagem: imgmacchiato
-  },
-  { 
-    id: 3, 
-    nome: 'FLAT WHITE',  
-    preco: 'R$ 19,20', 
-    imagem: imgflatwhite
-  },
-  { 
-    id: 4, 
-    nome: 'CAPPUCCINO',  
-    preco: 'R$ 15,60', 
-    imagem: imgcappuccino
-  }
-  
 
+const listaOriginal = [
+  { id: 1, nome: 'AMERICANO', preco: 'R$ 12,95', imagem: imgamericano },
+  { id: 2, nome: 'MACCHIATO', preco: 'R$ 20,75', imagem: imgmacchiato },
+  { id: 3, nome: 'FLAT WHITE', preco: 'R$ 19,20', imagem: imgflatwhite },
+  { id: 4, nome: 'CAPPUCCINO', preco: 'R$ 15,60', imagem: imgcappuccino }
 ]
+
+const listaCafes = ref([]) 
+
+onMounted(() => {
+  const salvo = localStorage.getItem('lista_mais_pedidos')
+  if (salvo) {
+    listaCafes.value = JSON.parse(salvo)
+  } else {
+    listaCafes.value = listaOriginal
+    localStorage.setItem('lista_mais_pedidos', JSON.stringify(listaOriginal))
+  }
+})
 </script>
 
 <template>
   <div class="page-container">
-    
     <h2 class="page-title">MAIS PEDIDOS</h2>
-    
     <div class="grid">
       <ProductCard 
         v-for="item in listaCafes" 
@@ -74,7 +63,7 @@ const listaCafes = [
 .grid {
   display: flex;
   flex-wrap: nowrap; 
-  gap: 20px;         
+  gap: 20px;          
   justify-content: center;
   align-items: stretch;
   width: 100%;
--- src/views/DiversosView.vue
+++ src/views/DiversosView.vue
@@ -1,39 +1,34 @@
 <script setup>
+import { ref, onMounted } from 'vue' 
 import ProductCard from '../components/ProductCard.vue'
 
 import img1 from '@/assets/img/produtos/img1.png'
 import img2 from '@/assets/img/produtos/img2.png'
 import img3 from '@/assets/img/produtos/img3.png'
 import img4 from '@/assets/img/produtos/img4.png'
-
 import graoBaixoDiversos from '@/assets/img/background/GraosBottomDiversos.png'
 
-const listaDiversos = [
-  { 
-    id: 1, 
-    nome: 'COOKIE +<br>CARAMEL MACCHIATO',  
-    preco: 'R$ 32,50', 
-    imagem: img1
-  },
-  { 
-    id: 2, 
-    nome: 'MACARONS +<br>FLAT WHITE',  
-    preco: 'R$ 30,10', 
-    imagem: img2
-  },
-  { 
-    id: 3, 
-    nome: 'PUDIM CARAMELIZADO +<br>LATTE',  
-    preco: 'R$ 19,20', 
-    imagem: img3
-  },
-  { 
-    id: 4, 
-    nome: 'KIDS: BOLO +<br>CAFÉ COM LEITE',  
-    preco: 'R$ 41,60', 
-    imagem: img4
-  }
+
+const dadosIniciais = [
+  { id: 1, nome: 'COOKIE +<br>CARAMEL MACCHIATO', preco: 'R$ 32,50', imagem: img1 },
+  { id: 2, nome: 'MACARONS +<br>FLAT WHITE', preco: 'R$ 30,10', imagem: img2 },
+  { id: 3, nome: 'PUDIM CARAMELIZADO +<br>LATTE', preco: 'R$ 19,20', imagem: img3 },
+  { id: 4, nome: 'KIDS: BOLO +<br>CAFÉ COM LEITE', preco: 'R$ 41,60', imagem: img4 }
 ]
+
+
+const listaDiversos = ref([])
+
+onMounted(() => {
+  const salvo = localStorage.getItem('combos_preferidos')
+  if (salvo) {
+    listaDiversos.value = JSON.parse(salvo)
+  } else {
+   
+    listaDiversos.value = dadosIniciais
+    localStorage.setItem('combos_preferidos', JSON.stringify(dadosIniciais))
+  }
+})
 </script>
 
 <template>
